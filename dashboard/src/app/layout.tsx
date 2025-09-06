af8f59d29e4b97feb8ac5287b107cb8e1f53050c
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2025-09-06 14:42:53 -0300

    feat:added several shadcn components

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
       >
         {children}
       </body>
--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold">Hello world!</h1>
+
+      <Button>Click me</Button>
     </main>
   );
 }
